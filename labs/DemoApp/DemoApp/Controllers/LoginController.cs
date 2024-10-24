413a5aa8d81b989c48b9f0039c9cfeaa4c63adf7
vikash-verma-profile <er.vikashverma@yahoo.com>
2024-10-24 13:17:32 +0530

    added code

---
+++ labs/DemoApp/DemoApp/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using DemoApp.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult SubmitLogin(Login login)
+        {
+            ProductDbContext dbContext = new ProductDbContext();
+            if(dbContext.Logins.Any(x=>x.UserName==login.UserName && x.Password==login.Password))
+            {
+                return RedirectToAction("Index", "Dashboard");
+            }
+            else
+            {
+                return View("Index");
+            }
+         
+        }
+        public IActionResult SubmitRegister(Login login)
+        {
+            ProductDbContext dbContext = new ProductDbContext();
+            dbContext.Logins.Add(login);
+            dbContext.SaveChanges();
+            return RedirectToAction("Index", "Login"); ;
+        }
+    }
+}
---
+++ labs/DemoApp/DemoApp/Model/Login.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DemoApp.Model;
+
+public partial class Login
+{
+    public int Id { get; set; }
+
+    public string UserName { get; set; } = null!;
+
+    public string? Password { get; set; }
+}
